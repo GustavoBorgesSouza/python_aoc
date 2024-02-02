fd9fbeb0060caa6a2d96db252b7d1be198372218
GustavoBorgesSouza <sgustavo.borges10@gmail.com>
2024-02-02 14:18:53 -0300

    added - second star and unit test

--- src/app.py
+++ src/app.py
@@ -1,22 +1,54 @@
-sum = 0
+import re
 
-file = open('day1_input.txt')
 
-lines = file.readlines()
 
-for i in range(lines.__len__()):
-    line = lines[i]
-    digitsLine = []
-    for j in range(line.__len__()):
-        if(line[j].isdigit()):
-            digitsLine.append(line[j])
-    if(digitsLine.__len__() == 1):
-        # print((str(digitsLine[0]) + str(digitsLine[0])))
-        sum += int(str(digitsLine[0]) + str(digitsLine[0]))
-    else:
-        lastIndex = digitsLine.__len__() - 1
-        # print(str(digitsLine[0]) + str(digitsLine[lastIndex]))
-        sum += int(str(digitsLine[0]) + str(digitsLine[lastIndex]))
-    # print(f'{line} && {digitsLine}')
+def estrelinha(fileName):
+    sum = 0
 
-print(sum)
+    with open(fileName, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    for i in range(lines.__len__()):
+        line = lines[i]
+        digitsLine = getDigits(line)
+        sum += int(str(digitsLine[0]) + str(digitsLine[-1]))
+        print(str(digitsLine[0]) + str(digitsLine[-1]))
+        # print(f'{line} && {digitsLine}')
+    print(str(len(lines)) + '    | AAAAAAAAAAAAAAAAAAAAAAAAAAA')
+    return sum
+
+def getDigits(string):
+        pattern = re.compile('(\d|one|two|three|four|five|six|seven|eight|nine)')
+        find = []
+
+        for c in range(len(string)):
+            result = pattern.match(string, c)
+            if(result is not None):
+                find.append(result.group())
+
+        for i, name in enumerate(find):
+            if(name == 'one'):
+                find[i] = 1
+            if(name == 'two'):
+                find[i] = 2
+            if(name == 'three'):
+                find[i] = 3
+            if(name == 'four'):
+                find[i] = 4
+            if(name == 'five'):
+                find[i] = 5
+            if(name == 'six'):
+                find[i] = 6
+            if(name == 'seven'):
+                find[i] = 7
+            if(name == 'eight'):
+                find[i] = 8
+            if(name == 'nine'):
+                find[i] = 9
+
+        print(string)
+        print(find)
+
+        return find
+
+print(estrelinha("src/day1_input.txt"))
--- tests/test_app.py
+++ tests/test_app.py
@@ -5,3 +5,8 @@ from src import app
 
 def test_bananinha():
     assert 42 == app.bananinnha()
+
+def test_estrelinha():
+    assert 281 == app.estrelinha(
+        "src/day1_example.txt"
+    )
